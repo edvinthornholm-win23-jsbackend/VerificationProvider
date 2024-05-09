5164bda620369d5edfcdb6a70f8d646f724e45b0
Edvin <edvin.thornholm@gmail.com>
2024-05-09 11:52:28 +0200

    GenerateVerificationCode done

---
+++ VerificationProvider/Data/Contexts/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VerificationProvider.Data.Entities;
+
+namespace VerificationProvider.Data.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext (options)
+{
+    public DbSet<VerificationRequestEntity> VerificationRequests { get; set; } = null!;
+
+
+    }
+
---
+++ VerificationProvider/Models/EmailRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificationProvider.Models;
+
+public class EmailRequest
+{
+    public string To { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string Body { get; set; } = null!;
+    public string PlainText { get; set; } = null!;
+}
